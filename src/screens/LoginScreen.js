860e8f03dcf9633b2ad6829a689ee2a2d2031b07
Leymar Andres Urbano Torres <leyurbano@gmail.com>
2025-03-16 20:57:03 -0500

    login_screen
    se agrega el screen con todos los estilos

---
+++ src/screens/LoginScreen.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import { View, Text, StyleSheet, Image } from 'react-native';
+
+
+const LoginScreen = () => {
+    return (
+        <View style={styles.container}>
+            <Image source={require('../assets/Logo.jpg')}
+            style={styles.logo}/>
+            <Image source={require('../assets/LogoName.jpg')}
+            style={styles.logo2}/>
+            <Text style={styles.title}> Iniciar <Text style={styles.title2}>Sesion</Text></Text>
+            
+            <View style={styles.footerContainer}>
+            <Text style={styles.footer}>Al ingresar, aceptas nuestros Terminos y condiciones,
+            y Politica de privacidad</Text>
+            </View>
+        </View>
+    );
+};
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#ffff',
+        padding: 10,
+    },
+    logo:{
+        width: 101, // Ajusta el ancho de la imagen
+        height: 99, // Ajusta la altura de la imagen
+        marginBottom: 0, // Espacio entre la imagen y el texto
+        resizeMode: 'contain'        
+    },
+
+    logo2:{
+        width: 207, // Ajusta el ancho de la imagen
+        height: 95, // Ajusta la altura de la imagen
+        marginBottom: 0, // Espacio entre la imagen y el texto
+        resizeMode: 'contain'        
+    },
+    
+
+    title: {
+        textAlign:'auto',
+        fontSize: 20,
+        fontWeight: 'bold',
+        margin: 0,
+      },
+    title2: {
+        textAlign:'auto',
+        fontSize: 24,
+        fontWeight: 'bold',
+        margin: 0,
+        color: '#236A34'
+    },
+    footerContainer:{
+
+        marginTop: 20,
+        width: '80%',
+        
+    },
+
+    footer:{
+        textAlign: 'center',
+        fontSize: 12,
+
+    },
+});
+
+export default LoginScreen;
