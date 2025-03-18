7d700ebe26e1eb6abc8553f3a6e84cf8a7049d7c
Leymar Andres Urbano Torres <leyurbano@gmail.com>
2025-03-18 00:23:36 -0500

    svg_login
    se modifica el componente svg y se agrega en el LoginScreen, según las indicaciones del mockup

--- src/components/CurvedBackground.js
+++ src/components/CurvedBackground.js
@@ -1,27 +1,26 @@
-import React from "react";
-import { View, StyleSheet } from "react-native";
-import Svg, { Path } from "react-native-svg";
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import Svg, { G, Path } from 'react-native-svg';
 
-const CurvedBackground = () => {
-  return (
-    <View style={styles.container}>
-      <Svg height="200" width="200" viewBox="0 0 200 200">
+const CurvedBackground = () => (
+  <View style={styles.container}>
+    <Svg width="180" height="251" viewBox="0 0 180 251" fill="none">
+      <G opacity="0.39">
         <Path
-          d="M 0 200 Q 50 100 200 0 L 0 0 Z"
-          fill="#A8C7A1" // Color verde claro
+          d="M10.7805 0.701664C10.7769 -4.84252 77.2808 48.2017 98.2808 88.2017C119.281 128.202 169.281 146.702 169.281 167.202C169.281 187.702 165.281 197.701 153.281 224.201L10.7805 224.201L10.7805 154.701C10.6194 56.1103 10.8075 42.0305 10.7805 0.701664Z"
+          fill="#236A34"
         />
-      </Svg>
-    </View>
-  );
-};
+      </G>
+    </Svg>
+  </View>
+);
 
 const styles = StyleSheet.create({
   container: {
-    position: "absolute",
-    bottom: 0,
-    left: 0,
-    width: 200,
-    height: 200,
+    
+    position: 'absolute',
+    bottom: -28,
+    left: -12,
   },
 });
 
--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -3,7 +3,6 @@ import { View, Text, StyleSheet, Image } from 'react-native';
 import LoginForm from '../components/LoginForm';
 import CurvedBackground from '../components/CurvedBackground';
 
-
 const LoginScreen = () => {
     return (
         <View style={styles.container}>
@@ -22,7 +21,6 @@ const LoginScreen = () => {
     );
 };
 
-
 const styles = StyleSheet.create({
     container: {
         flex: 1,
@@ -36,23 +34,20 @@ const styles = StyleSheet.create({
         width: 101, // Ajusta el ancho de la imagen
         height: 99, // Ajusta la altura de la imagen
         marginBottom: 0, // Espacio entre la imagen y el texto
-        resizeMode: 'contain'        
+        resizeMode: 'contain'
     },
-
     logo2:{
         width: 207, // Ajusta el ancho de la imagen
         height: 95, // Ajusta la altura de la imagen
         marginBottom: 0, // Espacio entre la imagen y el texto
-        resizeMode: 'contain'        
+        resizeMode: 'contain'
     },
-    
-
     title: {
         textAlign:'auto',
         fontSize: 20,
         fontWeight: 'bold',
         margin: 0,
-      },
+    },
     title2: {
         textAlign:'auto',
         fontSize: 24,
@@ -60,22 +55,16 @@ const styles = StyleSheet.create({
         margin: 0,
         color: '#236A34'
     },
-
     form:{
         alignItems: 'center',
     },
-
     footerContainer:{
-
         marginTop: 20,
         width: '80%',
-        
     },
-
     footer:{
         textAlign: 'center',
         fontSize: 12,
-
     },
 });
 
