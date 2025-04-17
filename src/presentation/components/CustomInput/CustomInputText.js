8a61b5152ca83960bb1fb15ebaad93f4550f77fd
leyurbano <leyurbano@gmail.com>
2025-04-16 22:41:52 -0500

    modifying CustomInputText component

--- src/presentation/components/CustomInput/CustomInputText.js
+++ src/presentation/components/CustomInput/CustomInputText.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, StyleSheet } from 'react-native';
+import { View, Text, TextInput, StyleSheet, TouchableOpacity } from 'react-native';
 
 
-const CustomInputText = ({ LabelText, PlaceholderText, HasError, SupportingText, IsDisabled, IsPassword }) => {
+const CustomInputText = ({ LabelText, PlaceholderText, HasError, SupportingText, IsDisabled, IsPassword, value, onChangeText,     showIcon = false,  icon = null,    onIconPress = () => {}
+}) => {
+ 
     const [isFocused, setIsFocused] = useState(false);
     
     return (
@@ -10,10 +12,12 @@ const CustomInputText = ({ LabelText, PlaceholderText, HasError, SupportingText,
             {/* Label encima del input */}
             <Text style={[styles.label, HasError && styles.labelError]}>{LabelText}</Text>
 
-            {/* Input */}
+            {/* Input con ícono a la derecha */}
+            <View style={styles.inputcontainer}>
             <TextInput 
                 style={[
                     styles.input, 
+                    showIcon && styles.inputWithIcon,  //icon
                     IsDisabled && styles.inputDisabled,
                     isFocused && !HasError && styles.inputFocused, // El focus tiene prioridad si no hay error
                     HasError && styles.inputError, // Si hay error, el borde es rojo sin importar el focus
@@ -22,10 +26,17 @@ const CustomInputText = ({ LabelText, PlaceholderText, HasError, SupportingText,
                 secureTextEntry={IsPassword} 
                 editable={!IsDisabled} 
                 placeholderTextColor="#B0B0B0"
+                value={value}
+                onChangeText={onChangeText}
                 onFocus={() => setIsFocused(true)} 
                 onBlur={() => setIsFocused(false)} 
             />
-            
+             {showIcon && (
+                    <TouchableOpacity onPress={onIconPress} style={styles.icon}>
+                        {icon}
+                    </TouchableOpacity>
+                )}
+            </View>
             {/* Texto de error debajo del input */}
             {HasError && <Text style={styles.errorText}>{SupportingText}</Text>}
         </View>
@@ -48,6 +59,10 @@ const styles = StyleSheet.create({
     labelError: {
         color: 'red',
     },
+    inputcontainer: {
+        position: 'relative',
+        justifyContent: 'center',
+    },
     input: {
         marginBottom: 5,
         width: "100%",
@@ -57,9 +72,19 @@ const styles = StyleSheet.create({
         borderRadius: 20,
         borderColor: "gray", // Color base
     },
+    inputWithIcon: {
+        paddingRight: 50, // Espacio para el ícono
+    },
+     icon: {
+        position: 'absolute',
+        right: 15,
+        height: '100%',
+        justifyContent: 'center',
+    },
     inputDisabled: {
         backgroundColor: '#E0E0E0',
     },
+   
     inputFocused: {
         borderColor: '#61CB7C', // Solo cambia el color sin tocar el grosor
         borderWidth: 2, // Mantiene el mismo tamaño para evitar bordes dobles
