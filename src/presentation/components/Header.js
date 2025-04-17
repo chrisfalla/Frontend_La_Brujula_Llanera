41f20c466d2bd9bf9f55df24461445af54c03421
leyurbano <leyurbano@gmail.com>
2025-04-16 22:44:14 -0500

    modifying Header

--- src/presentation/components/Header.js
+++ src/presentation/components/Header.js
@@ -1,15 +1,27 @@
 import React from 'react';
-import { View, StyleSheet, Image, Text } from 'react-native';
-
+import { View, StyleSheet, Image, Text, useWindowDimensions } from 'react-native';
 
 const Header = () => {
+    const { width } = useWindowDimensions(); // Obtener el ancho de la pantalla dinámicamente
+
     return (
         <View style={styles.header}>
             <Image source={require('../../shared/assets/CompassLogo.png')} style={styles.compass} />
             <View style={styles.containertitle}>
-                <Text style={styles.title3}>LA BRÚJULA <Text style={styles.title4}>LLANERA</Text></Text>
+                <Text
+                    style={[
+                        styles.title3,
+                        {
+                            fontSize: (width / 375) * 22, // Tamaño más grande, ajustado según el ancho
+                            lineHeight: (width / 375) * 30// Ajustar la altura de línea proporcionalmente
+                        },
+                    ]}
+                    numberOfLines={1} // Asegurar que el texto esté en una sola línea
+                >
+                    LA BRÚJULA <Text style={styles.title4}>LLANERA</Text>
+                </Text>
             </View>
-            <Image source={require('../../shared/assets/AvatarHeader.png')} style={styles.avatar}/>         
+            <Image source={require('../../shared/assets/AvatarHeader.png')} style={styles.avatar} />
         </View>
     );
 };
@@ -17,39 +29,37 @@ const Header = () => {
 const styles = StyleSheet.create({
     header: {
         flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        padding: 10,
+        justifyContent: 'space-between', // Mantener imágenes en los extremos
+        alignItems: 'center', // Centrado vertical
+        padding: 5,
         paddingTop: 35,
         backgroundColor: '#fff',
         width: '100%',
-        
     },
     compass: {
         width: 50,
         height: 50,
-        marginBottom: 10,
+        marginBottom: 0,
+        resizeMode: 'contain',
     },
-    containertitle:{
-        margin:10,
+    containertitle: {
+        flex: 1, // El título ocupa el espacio disponible
+        alignItems: 'center', // Centrado horizontal
+        marginHorizontal: 10,
     },
-
     title3: {
-        fontSize:20,
         fontWeight: '900',
-        margin: 0,
-        
+        textAlign: 'center',
     },
     title4: {
-        fontSize: 20,
         fontWeight: '900',
-        margin: 0,
         color: '#236A34',
     },
     avatar: {
         width: 50,
         height: 50,
-        marginBottom: 10,
+        marginBottom: 0,
+        resizeMode: 'contain',
     },
 });
 
