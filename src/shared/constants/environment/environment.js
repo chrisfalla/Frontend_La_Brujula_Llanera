8eecd1f38bdac03640c9611c365573b9b78ba2cf
leyurbano <leyurbano@gmail.com>
2025-04-24 10:51:00 -0500

    Create mock tags

--- src/shared/constants/environment/environment.js
+++ src/shared/constants/environment/environment.js
@@ -3,6 +3,8 @@
 const config = {
   BASE_URL: "https://api-dev.miapp.com",
   USE_MOCK_CATEGORIES: true, // activá/desactivá mocks desde acá
+  USE_MOCK_TAGS: true,
+  
 };
 
 export default config;
