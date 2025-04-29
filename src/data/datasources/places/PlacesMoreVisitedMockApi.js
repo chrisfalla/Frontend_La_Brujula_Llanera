f01d9905a453ef4eb82290560fa9eb51ac1945ee
Christofer Falla Villarreal <christoferfallavillarreal@gmail.com>
2025-04-29 13:14:46 -0500

    Update url mock

--- src/data/datasources/places/PlacesMoreVisitedMockApi.js
+++ src/data/datasources/places/PlacesMoreVisitedMockApi.js
@@ -9,8 +9,8 @@ export class PlaceMoreVisitedMockApi {
                 "name": "Catedral San José",
                 "idPlace": 2,
                 "visitCount": 3,
-                "imageUrl": null,
-                "imageCategoryId": null
+                "imageUrl": "https://ulgrkkcquytkafmoqwqt.supabase.co/storage/v1/object/sign/brujula-llanera/ejemplo/3.webp?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJicnVqdWxhLWxsYW5lcmEvZWplbXBsby8zLndlYnAiLCJpYXQiOjE3NDU4MjU4ODMsImV4cCI6MTc0ODQxNzg4M30.rWB1LeXO061Pd-1vb6abQFHC53cvV69w0TjDt_Ydzwk",
+                "imageCategoryId": 2
             },
             {
                 "name": "Centro Comercial Unicentro Yopal",
