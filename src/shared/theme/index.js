0a5e8d18ab7b0f4c1543f38c6cf6c78b586b2a72
Damiancaro09 <damiancaro2000@gmail.com>
2025-05-06 14:20:36 -0800

    (theme): define global styles (colors, fonts, borders, spacing) and install Poppins font

---
+++ src/shared/theme/index.js
@@ -0,0 +1,5 @@
+export * from './ColorsTheme';
+export * from './fontsTheme';
+export * from './spacingTheme';
+export * from './bordersTheme';
+export * from './textTheme';
---
+++ src/shared/theme/spacingTheme.js
@@ -0,0 +1,8 @@
+export const spacing = {
+
+    xs: 4,    // pequeño, usado para espacios mínimos
+    sm: 8,    // pequeño
+    md: 16,   // medio
+    lg: 24,   // grande
+    xl: 32,   // extra helkin
+};
