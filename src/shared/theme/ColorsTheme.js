de300b05320a17a03b911c6dabd0b5340fa1f754
Damiancaro09 <damiancaro2000@gmail.com>
2025-05-05 16:09:20 -0800

    feat(theme): agregar carpeta theme y archivo ColorsTheme.js con paleta de colores global

---
+++ src/shared/theme/ColorsTheme.js
@@ -0,0 +1,9 @@
+export const Colors = {
+    ColorPrimary: '#236A34',           
+    ColorOnPrimary: '#61CB7C',         
+    Black: '#000000',                  
+    LightGray: '#E0E0E0',              
+    DarkGray: '#BDBDBD',              
+    ErrorAdvertisingColor: '#E00E0E', 
+    background: '#FFFFFF',            
+};
