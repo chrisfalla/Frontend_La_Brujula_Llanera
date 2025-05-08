c35355564809f6dd77ed917bb8dcc3caf6de7b63
leyurbano <leyurbano@gmail.com>
2025-05-07 19:51:32 -0500

    Adding global styles to MainImage component

---
+++ src/presentation/components/MainImage/MainImage.js
@@ -0,0 +1,96 @@
+import React from 'react';
+import { View, Text, StyleSheet, ImageBackground } from 'react-native';
+import NavigationTopBar from '../NavigationTopBar/NavigationTopBar';
+import { TextStyles } from '../../styles/styles';
+
+const MainImage = ({ mainImage, name, category, onBackPress }) => {
+    return (
+        <View style={styles.container}>
+            <View style={styles.imageContainer}>
+                <ImageBackground
+                    source={{ uri: mainImage }}
+                    style={styles.mainImage}
+                    resizeMode="cover"
+                    imageStyle={styles.imageStyle}
+                >
+                    <View style={styles.contentContainer}>
+                        <NavigationTopBar
+                            onBackPress={onBackPress}
+                            useBackground={true}
+                            useHeart={true}
+                        />
+                        <View style={styles.infoContainer}>
+                            <Text style={styles.name}>{name}</Text>
+                            <Text style={styles.category}>{category}</Text>
+                        </View>
+                    </View>
+                </ImageBackground>
+            </View>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        width: '100%',
+        alignItems: 'center',
+        backgroundColor: '#fff',
+    },
+    imageContainer: {
+        width: '92%',
+        aspectRatio: 370 / 430,
+        borderRadius: 10, // Agregamos borderRadius al contenedor
+        overflow: 'hidden', // Necesario para que se vea el borderRadius
+        marginTop: '12.5%',
+        // Sombra para iOS
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 8, // Más pronunciada
+        },
+        shadowOpacity: 0.35, // Más opaca
+        shadowRadius: 12, // Más difusa
+        // Sombra para Android
+        elevation: 1, // Más alta
+        // backgroundColor: '#000', // Quitado para que la sombra no tape la imagen
+    },
+    mainImage: {
+        width: '100%',
+        height: '100%',
+    },
+    imageStyle: {
+        borderRadius: 10, // Agregamos borderRadius a la imagen
+    },
+    contentContainer: {
+        flex: 1,
+        marginTop: 14,
+        justifyContent: 'space-between',
+        borderRadius: 10, // Agregamos borderRadius al overlay
+        overflow: 'hidden',
+    },
+    infoContainer: {
+        padding: 10,
+        paddingBottom: 16,
+        paddingLeft: 15,
+        backgroundColor: 'rgba(0, 0, 0, 0.69)',
+       
+        
+    },
+    name: {
+        fontFamily: 'Poppins-SemiBold',
+        fontSize: 24,
+        color: '#fff',
+       
+    },
+    category: {
+        ...TextStyles.PoppinsRegular15,
+        color: '#fff',
+        opacity: 0.9,
+        left: 5,
+
+        
+       
+    },
+});
+
+export default MainImage;
