dab1a1b5bec148e00e0757c4c0a01ad83572b20e
chrisfalla <christoferfallavillarreal@gmail.com>
2025-05-10 02:47:44 -0500

    Update Component mostvisitedplaces with backend

--- src/presentation/components/MostVisitedPlaces/MostVisitedPlaces.js
+++ src/presentation/components/MostVisitedPlaces/MostVisitedPlaces.js
@@ -3,84 +3,87 @@ import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
 import PropTypes from 'prop-types';
 import { GlobalStyles, TextStyles, Colors } from '../../styles/styles';
 
-const MostVisitedPlaces = ({ place, onPress }) => {
-    return (
-        <TouchableOpacity style={styles.card} onPress={() => onPress(place)}>
-            <Image source={{ uri: place.imageUrl }} style={styles.image} resizeMode="cover" />
+const MostVisitedPlaces = ({ place, onPress, cardWidth, cardMargin }) => {
+  return (
+    <TouchableOpacity
+      style={[styles.card, { width: cardWidth, marginRight: cardMargin }]} // Usamos cardMargin
+      onPress={() => onPress(place)}
+    >
+      <Image source={{ uri: place.imageUrl }} style={styles.image} resizeMode="cover" />
 
-            <View style={styles.labelContainer}>
-                <Text style={styles.labelText}>{place.placeName}</Text>
-            </View>
+      <View style={styles.labelContainer}>
+        <Text style={styles.labelText} numberOfLines={1} ellipsizeMode="tail">
+          {place.placeName}
+        </Text>
+      </View>
 
-            <View style={styles.button}>
-                <Text style={styles.buttonText}>Ver más</Text>
-            </View>
-        </TouchableOpacity>
-    );
+      <View style={styles.button}>
+        <Text style={styles.buttonText}>Ver más</Text>
+      </View>
+    </TouchableOpacity>
+  );
 };
 
 MostVisitedPlaces.propTypes = {
-    place: PropTypes.shape({
-        idPlace: PropTypes.number.isRequired,
-        placeName: PropTypes.string.isRequired,
-        imageUrl: PropTypes.string.isRequired,
-    }).isRequired,
-    onPress: PropTypes.func.isRequired,
+  place: PropTypes.shape({
+    idPlace: PropTypes.number.isRequired,
+    placeName: PropTypes.string.isRequired,
+    imageUrl: PropTypes.string.isRequired,
+  }).isRequired,
+  onPress: PropTypes.func.isRequired,
+  cardWidth: PropTypes.number.isRequired,
+  cardMargin: PropTypes.number.isRequired, // Nueva prop
 };
 
 const styles = StyleSheet.create({
-    card: {
-        ...GlobalStyles.borderRadius,
-        ...GlobalStyles.elevation,
-        width: 350,
-        height: 200,
-        borderColor: '#fff',
-        borderWidth: 3,
-        marginRight: 10,
-        overflow: 'hidden',
-    },
-    image: {
-        width: '100%',
-        height: '100%',
-    },
-    labelContainer: {
-        position: 'absolute',
-        top: 16,
-        left: 0,
-        backgroundColor: Colors.ColorPrimary,
-        borderTopRightRadius: 12,
-        borderBottomRightRadius: 12,
-        paddingHorizontal: 16,
-        paddingVertical: 6,
-        maxWidth: '80%',
-        height: 40,
-        justifyContent: 'center',
-    },
-    labelText: {
-        ...TextStyles.PoppinsSemiBold15,
-        color: '#fff',
-        fontSize: 16,
-        fontWeight: '600',
-    },
-    button: {
-        position: 'absolute',
-        bottom: 16,
-        alignSelf: 'center',
-        backgroundColor: 'rgba(0, 0, 0, 0.6)',
-        borderRadius: 8,
-        paddingVertical: 0,
-        width: '30%',
-        height: '13%',
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    buttonText: {
-        ...TextStyles.PoppinsSemibold13,
-        color: '#fff',
-        fontSize: 14,
-        fontWeight: '600',
-        textAlign: 'center',
-    },
+  card: {
+    borderRadius: GlobalStyles.borderRadius,
+    elevation: GlobalStyles.elevation,
+    height: 209,
+    borderColor: '#fff',
+    borderWidth: 3,
+    overflow: 'hidden',
+    marginBottom: 6,
+    
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+    // objectFit: '',
+  },
+  labelContainer: {
+    position: 'absolute',
+    top: 10,
+    backgroundColor: Colors.ColorPrimary,
+    borderTopRightRadius: 10,
+    borderBottomRightRadius: 10,
+    paddingHorizontal: 16,
+    paddingVertical: 6,
+    maxWidth: '80%',
+    height: 40,
+    justifyContent: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 9,
+  },
+  labelText: {
+    ...TextStyles.PoppinsSemiBold15,
+    color: '#fff',
+  },
+  button: {
+    position: 'absolute',
+    bottom: 16,
+    alignSelf: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.6)',
+    borderRadius: GlobalStyles.borderRadius,
+    paddingVertical: 2,
+    paddingHorizontal: 18,
+  },
+  buttonText: {
+    ...TextStyles.PoppinsSemibold13,
+    color: '#fff',
+    marginTop: 2,
+  },
 });
 
 export default MostVisitedPlaces;
