b56f9e6e0bbd25b3bf007f56ce4f57ba88094713
Damiancaro09 <damiancaro2000@gmail.com>
2025-05-21 20:55:26 -0800

    Created PlaceReviewScreen

--- src/presentation/screens/Reviews/PlaceReviewsScreen.js
+++ src/presentation/screens/Reviews/PlaceReviewsScreen.js
@@ -42,71 +42,67 @@ const handleSubmitReview = () => {
 };
 
 return (
-    <View style={styles.container}>
+<View style={styles.container}>
     <NavigationTopBar
         primaryIcon="chevron-back"
         onBackPress={() => navigation.goBack()}
         title="Magdalena - Comentarios"
         removeBackground={true}
     />
-    <View style={styles.ratingcontainer}>
+<View style={styles.ratingcontainer}>
     <View style={styles.ratingcard}>
         <Text style={styles.text}>Calificación General</Text>
         <Text style={styles.score}>4.3</Text>
-
     </View>
-        <View style= {styles.rating}>
+    <View style= {styles.rating}>
         <Rating average={4.3} />
-        </View>
     </View>
+</View>
 
-    <View style={styles.buttoncontainer}>
-        
-        <CustomButton
-            titletext="Añadir Review"
-            onPress={handleAddReview}
-            type="Primary"
-            size="Small"
-        />
-        
-    </View>
+<View style={styles.buttoncontainer}>
+    <CustomButton
+        titletext="Añadir Review"
+        onPress={handleAddReview}
+        type="Primary"
+        size="Small"
+    />
+</View>
 
-    {/* Condicional: si estamos añadiendo review, mostramos el input y botón */}
-    {isAddingReview ? (
-        <View style={styles.reviewform}>
-
-        <CustomInputText
-            LabelText={'Ingresa su review'}
-            PlaceholderText={'comentario.....'}
-            HasError={false}
-            value={newComment}
-            onChangeText={setNewComment}
-        />
-        <View style= {styles.ratingsecondary}> 
+{/* Condicional: si estamos añadiendo review, mostramos el input y botón */}
+{isAddingReview ? (
+<View style={styles.reviewform}>
+    <CustomInputText
+        LabelText={'Ingresa su review'}
+        PlaceholderText={'comentario.....'}
+        HasError={false}
+        value={newComment}
+        onChangeText={setNewComment}
+    />
+    <View style= {styles.ratingsecondary}> 
         <Rating average={rating} onChange={setRating} />
-        </View>
+    </View>
 
-        <CustomButton
-            titletext="Guardar review"
-            onPress={handleSubmitReview}
-            type="Secondary"
-            size="Small"
-        />
-        </View>
+    <CustomButton
+        titletext="Guardar review"
+        onPress={handleSubmitReview}
+        type="Secondary"
+        size="Small"
+    />
+</View>
     ) : (
-        <FlatList
+    <FlatList
         data={reviews}
         keyExtractor={(item) => item.id.toString()}
         renderItem={({ item }) => (
-        <ReviewCard
-            username={item.username}
-            comment={item.comment}
-            date={item.date}
-            />
-        )}
-        />
+    <ReviewCard
+        username={item.username}
+        comment={item.comment}
+        date={item.date}
+    />
     )}
-    </View>
+    />
+    )}
+</View>
 );
 };
 
@@ -124,48 +120,46 @@ ratingcontainer: {
     
 },
 
-  ratingcard: {
+ratingcard: {
     paddingVertical: 30,
     alignItems: 'center',
     // sombra Android
     elevation: 3,
     borderRadius: 4,
+},
 
-  },
-
-  rating: {
+rating: {
     position: 'absolute',
     top: 153,
     alignSelf: 'center',
+},
 
-  },
-
-  text: {
+text: {
     ...TextStyles.PoppinsSemiBold15,
     color: Colors.DarkGray,
     marginBottom: 10,
-  },
+},
 
-  score: {
+score: {
     fontWeight: 'bold',
     color: Colors.ColorPrimary,
     fontSize: 60,
-  },
+},
 
-  buttoncontainer: {
+buttoncontainer: {
     marginTop: 60,
     marginBottom: 40,
     alignItems: 'center',
-  },
+},
 
-  reviewform: {
+reviewform: {
     alignItems: 'center',
     paddingHorizontal: 20,
-  },
+},
 
-  ratingsecondary: {
+ratingsecondary: {
     marginVertical: 20,
-  },
+},
 
 });
 
